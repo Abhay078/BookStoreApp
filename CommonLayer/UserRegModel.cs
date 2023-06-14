ac4b4818072bf73fabb016c88f3e40ceef13f249
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:21:57 +0530

    [Abhay]-User registration data for payload data

---
+++ CommonLayer/UserRegModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class UserRegModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string MobileNumber { get; set; }
+    }
+}
