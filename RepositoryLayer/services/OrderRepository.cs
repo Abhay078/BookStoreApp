4e9101fdff67ec8b3b4585c3217e73f8015f9220
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:37:40 +0530

    [Abhay]-repository file of order

---
+++ RepositoryLayer/services/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using CommonLayer;
+using RepositoryLayer.BookDbContext;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace RepositoryLayer.services
+{
+    public class OrderRepository:IOrderRepository
+    {
+        private readonly BookContext context;
+        public OrderRepository(BookContext context)
+        {
+            this.context = context;
+        }
+        public OrderEntity AddOrder(List<int> BookIds,long UserId)
+        {
+            try
+            {
+                OrderEntity entity = new OrderEntity();
+                entity.UserId = UserId;
+                entity.CreatedDate = DateTime.Now;
+
+                foreach (int id in BookIds)
+                {
+                    var book = context.Book.Find(id);
+                    if (book != null)
+                    {
+                        entity.Books.Add(book);
+                    }
+
+                }
+                context.Order.Add(entity);
+                context.SaveChanges();
+                return entity;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
