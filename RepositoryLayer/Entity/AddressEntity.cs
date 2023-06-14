f6ce79336dd9700174f6ce29b50dc340c970d9f6
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:24:10 +0530

    [Abhay]-address class for database table

---
+++ RepositoryLayer/Entity/AddressEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class AddressEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long AddressId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string type { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+
+        public UserEntity UserEntity { get; set; }
+    }
+}
