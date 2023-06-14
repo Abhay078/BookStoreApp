2a69efa62b1f043eb637834c4b315e2ed4de6326
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:19:29 +0530

    [Abhay]-add book model for payload data

---
+++ CommonLayer/AddBookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class AddBookModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public long Price { get; set; }
+        public long DiscountedPrice { get; set; }
+        public string BookImage { get; set; }
+        public int Quantity { get; set; }
+    }
+}
