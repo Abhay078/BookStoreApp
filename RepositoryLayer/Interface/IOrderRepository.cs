c585f41953bdf3c46030fa000cbdb8bce659267a
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:27:59 +0530

    [Abhay]-interface for order repo

---
+++ RepositoryLayer/Interface/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IOrderRepository
+    {
+        public OrderEntity AddOrder(List<int> BookIds, long UserId);
+    }
+}
