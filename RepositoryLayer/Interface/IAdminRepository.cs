6b0c370531e74e2de35b83252472d66aaf64b767
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:27:25 +0530

    [Abhay]-interface for admin and book repo

---
+++ RepositoryLayer/Interface/IAdminRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IAdminRepository
+    {
+        public AdminEntity AdminRegister(UserRegModel model);
+        public string EncryptPassword(string password);
+        public AdminEntity Login(LoginModel login);
+        public string Generate(string Email, long AdminId, string Role);
+    }
+}
---
+++ RepositoryLayer/Interface/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IBookRepository
+    {
+        public BookEntity AddBook(AddBookModel model);
+        public Boolean DeleteBook(int bookId);
+        public BookEntity UpdateBook(AddBookModel model, int bookId);
+        public IEnumerable<BookEntity> GetBooks();
+    }
+}
