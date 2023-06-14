d422b8d2d70da41da8e4298f16755b3296b59e14
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:24:49 +0530

    [Abhay]-Book model class for database table

---
+++ RepositoryLayer/Entity/BookEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class BookEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public long Price { get; set; }
+        public long DiscountedPrice { get; set; }
+        public string BookImage { get; set; }
+        public int Quantity { get; set; }
+    }
+}
