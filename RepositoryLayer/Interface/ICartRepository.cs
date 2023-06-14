b1e3ce70142aa728388927582ccc9546d61abee5
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:27:45 +0530

    [Abhay]-interface for cart repo

---
+++ RepositoryLayer/Interface/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ICartRepository
+    {
+        public CartEntity AddItemToCart(AddCartItemModel model, long UserId);
+        public Boolean DeleteItem(long ItemId);
+        public List<CartEntity> GetItems(long UserId);
+        public CartEntity UpdateQuantity(long ItemId, int quantity);
+    }
+}
