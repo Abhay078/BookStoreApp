3679673d93ead247ec49eac4de041b860eba0b4b
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:20:08 +0530

    [Abhay]-add order model for payload data

---
+++ CommonLayer/AddOrderModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class AddOrderModel
+    {
+        public DateTime CreatedDate { get; set; }
+        public List<int> BookIds { get; set; }
+    }
+}
