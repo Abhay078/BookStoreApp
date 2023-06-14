31f29ed184a97db4d26c63723b3aa1a6ee3a0069
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:25:25 +0530

    [Abhay]-order model class for database table

---
+++ RepositoryLayer/Entity/OrderEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class OrderEntity
+    {
+        public OrderEntity()
+        {
+            Books= new List<BookEntity>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        public ICollection<BookEntity> Books { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+        public UserEntity UserEntity { get; set;}
+
+    }
+}
