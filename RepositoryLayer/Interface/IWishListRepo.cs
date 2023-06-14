cf0e69800309f325180e3ab0769e012457307c85
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:28:34 +0530

    [Abhay]-interface for wishlist repo

---
+++ RepositoryLayer/Interface/IWishListRepo.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IWishListRepo
+    {
+        public WishListEntity AddItem(int BookId, long UserId);
+        public Boolean DeleteItem(long ItemId);
+        public List<WishListEntity> GetAll();
+    }
+}
