644444047da86699a7543bfbf69232567755693b
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:26:01 +0530

    [Abhay]-wish List model class for database table

---
+++ RepositoryLayer/Entity/WishListEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class WishListEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ItemId { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+        public UserEntity UserEntity { get; set; }
+        public int BookId { get; set; }
+
+        [ForeignKey("BookId")]
+        public BookEntity BookEntity { get; set; }
+
+    }
+}
