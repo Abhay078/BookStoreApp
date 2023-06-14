265b93ffe6d0890e1ad39b496c7a9638c637ca48
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:20:50 +0530

    [Abhay]-add item model for payload data

---
+++ CommonLayer/AddWishItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class AddWishItemModel
+    {
+        public long UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
