15f4a2ca02b35d7a14a9d089a60774ee0a418b14
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:38:21 +0530

    [Abhay]-repository file of wishlist

---
+++ RepositoryLayer/services/WishListRepo.cs
@@ -0,0 +1,70 @@
+﻿using CommonLayer;
+using RepositoryLayer.BookDbContext;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.services
+{
+    public class WishListRepo:IWishListRepo
+    {
+        private readonly BookContext context;
+        public WishListRepo(BookContext context)
+        {
+            this.context = context;
+        }
+        public WishListEntity AddItem(int BookId,long UserId)
+        {
+            try
+            {
+                WishListEntity Item = new WishListEntity();
+                Item.BookId = BookId;
+                Item.UserId = UserId;
+                context.WishList.Add(Item);
+                context.SaveChanges();
+                return Item;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+        }
+        public Boolean DeleteItem(long ItemId)
+        {
+            try
+            {
+                var check=context.WishList.FirstOrDefault(x=>x.ItemId==ItemId);
+                if(check!=null)
+                {
+                    context.WishList.Remove(check);
+                    context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public List<WishListEntity> GetAll()
+        {
+            try
+            {
+                return context.WishList.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
