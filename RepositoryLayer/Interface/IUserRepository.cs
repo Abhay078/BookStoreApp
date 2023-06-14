bd48d5eab579d316113737cb9019df4b1a05a3da
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:28:14 +0530

    [Abhay]-interface for user repo

---
+++ RepositoryLayer/Interface/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IUserRepository
+    {
+        public UserEntity UserRegister(UserRegModel model);
+        public UserEntity Login(LoginModel login);
+        public string Generate(string Email, long Userid,string role);
+        public AddressEntity AddAddress(AddAddressModel model, long UserId);
+        public AddressEntity UpdateAddress(AddAddressModel model, long AddressId);
+    }
+}
