2346eb7fc54276eb715510a143ee65f0611996a9
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:25:05 +0530

    [Abhay]-cart model class for database table

---
+++ RepositoryLayer/Entity/CartEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class CartEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long CartItemId { get; set; }
+        public int Quantity { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+
+        public UserEntity UserEntity { get; set; }
+        public int BookId { get; set; }
+        [ForeignKey("BookId")]
+        public BookEntity BookEntity { get; set; }
+
+
+        
+    }
+}
