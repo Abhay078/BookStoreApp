cf3e705f69a32810d2ef444ac532078376acdd71
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-14 13:23:12 +0530

    [Abhay]-Db context class for BookStore

---
+++ RepositoryLayer/BookDbContext/BookContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.BookDbContext
+{
+    public class BookContext:DbContext
+    {
+        public BookContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+
+        }
+        public DbSet<UserEntity> User { get; set; }
+        public DbSet<AdminEntity> Admin { get; set; }
+        public DbSet<BookEntity> Book { get; set; }
+        public DbSet<WishListEntity> WishList { get; set; }
+        public DbSet<OrderEntity> Order { get; set; }
+        public DbSet<AddressEntity> Address { get; set; }
+        public DbSet<CartEntity> Cart { get; set; }
+    }
+}
