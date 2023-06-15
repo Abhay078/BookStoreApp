5d70cd9e963261ad4d4040ec622725736de0393a
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-15 20:31:32 +0530

    [Abhay]-Interface for manager Book

---
+++ ManagerLayer/Interface/IBookManager.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IBookManager
+    {
+        public BookEntity AddBook(AddBookModel model);
+        public Boolean DeleteBook(int bookId);
+        public BookEntity UpdateBook(AddBookModel model, int bookId);
+        public IEnumerable<BookEntity> GetBooks();
+    }
+}
