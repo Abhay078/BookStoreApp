9642ed3843d08c408c5dd9d23714a95015b5a8f5
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-15 20:31:03 +0530

    [Abhay]-Interface for manager Cart

---
+++ ManagerLayer/Interface/ICartManager.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface ICartManager
+    {
+        public CartEntity AddItemToCart(AddCartItemModel model, long UserId);
+        public Boolean DeleteItem(long ItemId);
+        public List<CartEntity> GetItems(long UserId);
+        public CartEntity UpdateQuantity(long ItemId, int quantity);
+    }
+}
