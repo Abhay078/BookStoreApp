4c786e4ea36ccd12da1e19935166c9e1bb0a85fb
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-15 20:30:30 +0530

    [Abhay]-Interface for manager user

---
+++ ManagerLayer/Interface/IUserManager.cs
@@ -0,0 +1,18 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IUserManager
+    {
+        public UserEntity UserRegister(UserRegModel model);
+        public UserEntity Login(LoginModel login);
+        public string Generate(string Email, long Userid,string role);
+        public AddressEntity AddAddress(AddAddressModel model,long UserId);
+        public AddressEntity UpdateAddress(AddAddressModel model, long AddressId);
+
+    }
+}
