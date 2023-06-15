171149bd95c0f5efaad38088099cbee090c42e83
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-15 20:30:14 +0530

    [Abhay]-Interface for manager admin

---
+++ ManagerLayer/Interface/IAdminManager.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IAdminManager
+    {
+        public AdminEntity AdminRegister(UserRegModel model);
+        public string EncryptPassword(string password);
+        public AdminEntity Login(LoginModel login);
+        public string Generate(string Email, long AdminId, string Role);
+    }
+}
