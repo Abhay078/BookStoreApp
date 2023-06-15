a9602c71fee0f5382737132ff098801c5d0ceadb
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-15 20:30:50 +0530

    [Abhay]-Interface for manager wishlist

---
+++ ManagerLayer/Interface/IWishListManager.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IWishListManager
+    {
+        public WishListEntity AddItem(int BookId, long UserId);
+        public Boolean DeleteItem(long ItemId);
+        public List<WishListEntity> GetAll();
+    }
+}
