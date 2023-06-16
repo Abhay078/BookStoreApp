a8de45c7ec1da33be0ffd19cd25dc20607cbe197
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-16 17:50:03 +0530

    [Abhay]-manager class for wishlist

---
+++ ManagerLayer/Services/WishListManager.cs
@@ -0,0 +1,34 @@
+﻿using CommonLayer;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class WishListManager:IWishListManager
+    {
+        private readonly IWishListRepo repo;
+        public WishListManager(IWishListRepo repo)
+        {
+            this.repo = repo;
+        }
+
+        public WishListEntity AddItem(int BookId, long UserId)
+        {
+            return this.repo.AddItem( BookId,UserId);
+        }
+
+        public bool DeleteItem(int BookId)
+        {
+           return this.repo.DeleteItem(BookId);
+        }
+
+        public List<WishListEntity> GetAll()
+        {
+            return this.repo.GetAll();
+        }
+    }
+}
