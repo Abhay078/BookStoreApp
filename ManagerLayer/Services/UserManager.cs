3670c9b9b16e5cf56f6bcd63ad3a0bd2f0ffdb1b
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-16 17:49:48 +0530

    [Abhay]-manager class for user

---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using CommonLayer;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager:IUserManager
+    {
+        private readonly IUserRepository User;
+        public UserManager(IUserRepository User) 
+        {
+            this.User = User;
+        }
+        public UserEntity UserRegister(UserRegModel model)
+        {
+            return this.User.UserRegister(model);
+        }
+        public UserEntity Login(LoginModel login)
+        {
+            return this.User.Login(login);
+        }
+        public string Generate(string Email, long Userid,string role)
+        {
+            return this.User.Generate(Email, Userid,role);
+        }
+
+        public AddressEntity AddAddress(AddAddressModel model,long UserId)
+        {
+           return this.User.AddAddress(model,UserId);
+        }
+
+        public AddressEntity UpdateAddress(AddAddressModel model, long AddressId)
+        {
+            return this.User.UpdateAddress(model, AddressId);
+        }
+
+        public AddressEntity GetAddress(long UserId)
+        {
+            return this.User.GetAddress(UserId);
+        }
+    }
+}
