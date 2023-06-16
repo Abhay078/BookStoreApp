6294af0242b67a75b064f31f8697b2a550269ba1
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-16 17:48:55 +0530

    [Abhay]-manager class for admin

---
+++ ManagerLayer/Services/AdminManager.cs
@@ -0,0 +1,38 @@
+﻿using CommonLayer;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class AdminManager : IAdminManager
+    {
+        private readonly IAdminRepository adminRepository;
+        public AdminManager(IAdminRepository adminRepository) 
+        {
+            this.adminRepository = adminRepository;
+        }
+        public AdminEntity AdminRegister(UserRegModel model)
+        {
+            return this.adminRepository.AdminRegister(model);
+        }
+
+        public string EncryptPassword(string password)
+        {
+            return this.adminRepository.EncryptPassword(password);
+        }
+
+        public string Generate(string Email, long AdminId, string Role)
+        {
+            return this.adminRepository.Generate(Email, AdminId, Role);
+        }
+
+        public AdminEntity Login(LoginModel login)
+        {
+            return this.adminRepository.Login(login);
+        }
+    }
+}
