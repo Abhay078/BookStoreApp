ac55de84ec473f3219ec37a44e23464fd5cdb35a
Abhay078 <abhaysrivastava078@gmail.com>
2023-06-16 17:49:35 +0530

    [Abhay]-manager class for order

---
+++ ManagerLayer/Services/OrderManager.cs
@@ -0,0 +1,23 @@
+﻿using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class OrderManager : IOrderManager
+    {
+        private readonly IOrderRepository repository;
+        public OrderManager(IOrderRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public OrderEntity AddOrder(List<int> BookIds, long UserId)
+        {
+            return this.repository.AddOrder(BookIds, UserId);
+        }
+    }
+}
